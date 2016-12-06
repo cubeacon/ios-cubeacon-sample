97baaed4d778658eac910e2473172470368571be
Cubeacon <eyro.cubeacon@gmail.com>
2016-12-06 14:17:11 +0700

    Update v2.0.0

---
+++ CubeaconSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CubeaconSample
+//
+//  Created by Eyro Labs on 12/6/16.
+//  Copyright © 2016 Eyro Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
